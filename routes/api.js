8421463eb9d78e115fcbe91c20609520f5137a29
Katie McBride <mcbride.katieb@gmail.com>
2020-01-18 12:51:17 -0600

    api.js outline

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const Workout = require('../models/workout');
+
+
+// Add a new workout 
+// Workout is a date with an array of exercises
+router.post('/api/workouts', (req, res) =>{
+    Workout.create({})
+        .then(dbworkout =>{
+            res.json(dbworkout);
+        })
+        .catch(err =>{
+            res.json(err);
+        });
+});
+
+// ADD exercises to a work out put route /api/wourkouts/:id
+
+
+// DELETE workout /api/workouts
+
+
+// GET all workouts /api/workouts
+
+
+// GET specific workout (get the last 7 workouts) /api/workouts/range
+
+
+module.exports = router;
---
+++ routes/html.js
@@ -0,0 +1,4 @@
+// route for exercise.html
+
+// route for stats.html
+
